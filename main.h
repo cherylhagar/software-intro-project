be8b422599f5b8a1a76ba9b1045dc897ae0c1257
Chris <chris.johnson1479@gmail.com>
2021-09-06 22:44:02 -0700

    Add new file main.h

---
+++ main.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cstdlib>
+#include <stdlib.h>
+//#include <time.h> used to prototype I used rand() to generate random values while building my linked list so I didn't have to keep entering values I could just have them randomly made.
+#include <iostream>
+#include "node.h"	//linking 
+#include "linked_list.h"	//linking
+
+using namespace std;
+/**
+ * @breif 	Creates a space in memory for the program to run in 
+ */
+int menu();
+void clearScreen();
+void pauseScreen();
+//declaring forward declarations to initialize before calling in main
