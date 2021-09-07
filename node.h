66db7cb3434977b47668ddda412738ebb9f81146
Chris <chris.johnson1479@gmail.com>
2021-09-06 22:45:25 -0700

    Add new file node.h

---
+++ node.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <cstdlib>
+
+/**
+ * @class Node Node.h This class describes a node.
+ * This class creates nodes to insert and link into the list.
+ */
+class Node
+{
+public:
+	int data = 0;	//initialized to zero	
+	Node* next = NULL; // initialized to NULL whenever an instace is generated.
+	Node(void);	//default constructor
+
+private:
+	
+	
+};
