635408ac4462c77a3e41194fccb1e476263e6a9c
Zack Mudd <zmudd@serranosystems.com>
2021-09-15 17:21:54 -0700

    Fix unused variable warnings in node.c
    Declarations and initializations of the next and data fields of the Node
    were backwards, with the declaration in the source file and the
    initialization in the header. This is now fixed.

--- node.cpp
+++ node.cpp
@@ -3,6 +3,7 @@
  * @brief      Constructs a new instance of node with value and next ptr
  */
 Node::Node(void) {	//default constructor
-	int data;	//value where numbers are being stored
-	Node* next;	//using to link to the next value in the linked list
+	/* initialize elements to null values */
+	data = 0;
+	next = NULL;
 }
